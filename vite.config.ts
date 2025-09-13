0d42004602fe45b2e4075d199d39fa447b04d8a1
donhulam <donhulam@gmail.com>
2025-09-13 20:48:57 +0700

    Lần 3

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: 3000
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 3000
     }
   };
 });
